ebffa05978fe92b0c111e3da5794271a22d8497e
Esther Kim <kim.esther117@gmail.com>
2023-01-23 16:33:19 -0800

    Connected URL settings and built Django Models

--- sales/api/sales_rest/models.py
+++ sales/api/sales_rest/models.py
@@ -1,3 +1,57 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class AutomobileVO(models.Model):
+    vin = models.CharField(max_length=17, unique=True)
+
+    def __str__(self):
+        return self.vin
+
+    def get_api_url(self):
+        return reverse("api_automobile", kwargs={"vin": self.vin})
+
+
+class SalesPerson(models.Model):
+    name = models.CharField(max_length=100)
+    employee_number = models.PositiveIntegerField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_api_url(self):
+        return reverse("api_salesperson", kwargs={"pk": self.id})
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=25, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_api_url(self):
+        return reverse("api_customer", kwargs={"pk": self.id})
+
+
+class SaleRecord(models.Model):
+    sales_person = models.ForeignKey(
+        SalesPerson,
+        related_name="sales_records",
+        on_delete=models.PROTECT,
+    )
+    customer = models.ForeignKey(
+        Customer,
+        related_name="sales_records",
+        on_delete=models.PROTECT,
+    )
+    automobile = models.ForeignKey(
+        AutomobileVO,
+        related_name="sales_records",
+        on_delete=models.PROTECT,
+    )
+    sales_price = models.DecimalField(max_digits=None, decimal_places=2)
+
+    def __str__(self):
+        return self.sales_person
