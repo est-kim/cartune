9c311131e77e6f9f232bb41328c3aa7c613b8fb2
Esther Kim <kim.esther117@gmail.com>
2023-01-24 15:50:41 -0800

    Built React JSX SalesPersonsList

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,14 +1,39 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import React, { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import Nav from './Nav';
+import SalesPersonsList from './SalesPersonsList';
 
 function App() {
+  const [salesPersons, setSalesPersons] = useState([])
+
+  const getSalesPersons = async () => {
+    const url = 'http://localhost:8090/api/salespersons/'
+    const response = await fetch(url);
+
+    if (response.ok) {
+      const data = await response.json();
+      const salesPersons = data.sales_persons
+      setSalesPersons(salesPersons)
+    }
+  }
+
+  useEffect( () => {
+    getSalesPersons();
+  }, [
+    setSalesPersons,
+  ])
+
   return (
     <BrowserRouter>
       <Nav />
       <div className="container">
         <Routes>
-          <Route path="/" element={<MainPage />} />
+          <Route index element={<MainPage />} />
+          <Route path="salespersons">
+            <Route path="" element={<SalesPersonsList salesPersons={salesPersons} getSalesPersons={getSalesPersons} />} />
+            {/* <Route path="new" element={<SalesPersonForm getSalesPersons={getSalesPersons} />} /> */}
+          </Route>
         </Routes>
       </div>
     </BrowserRouter>
