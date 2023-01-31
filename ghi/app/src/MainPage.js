ec614b39f61da2c9689dbc8cc1f5f5a99f45be2d
Jason Dai <jasondai91780@gmail.com>
2023-01-30 16:14:22 -0800

    Pushing to pull changes.

--- ghi/app/src/MainPage.js
+++ ghi/app/src/MainPage.js
@@ -1,11 +1,11 @@
 function MainPage() {
   return (
     <div className="px-4 py-5 my-5 text-center">
-      <h1 className="display-5 fw-bold">CarCar</h1>
+      <img src="./cartune_logo.png" alt="CarTune logo"/>
+      <h1 className="display-5 fw-bold">CarTune</h1>
       <div className="col-lg-6 mx-auto">
         <p className="lead mb-4">
-          The premiere solution for automobile dealership
-          management!
+          Tune in to your next favorite car.
         </p>
       </div>
     </div>
--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -2,16 +2,16 @@ import { NavLink, Link } from 'react-router-dom';
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-success">
+    <nav className="navbar navbar-expand-lg navbar-light" style={{ backgroundColor:"#fafae9" }}>
       <div className="container-fluid">
-        <NavLink className="navbar-brand" to="/">CarCar</NavLink>
+        <NavLink className="navbar-brand" to="/"><img src="./cartune_logo.png" alt="CarTune logo" width="125px"/></NavLink>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
           <div className="collapse navbar-collapse" id="navbarNavDarkDropdown">
-              <ul className="navbar-nav">
+              <ul className="navbar-nav" style={{ color: "black" }}>
                 <li className="nav-item dropdown">
                   <a className="nav-link dropdown-toggle" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                     Appointments
